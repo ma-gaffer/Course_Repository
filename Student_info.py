b6bcb090acd54cc7d721e3223576afb9e36a5881
ma-gaffer <markflaviandube12@gmail.com>
2026-07-23 23:38:46 +0200

    add lesson 1 and student info task

---
+++ Student_info.py
@@ -0,0 +1,6 @@
+first_name = input("what is your name:")
+surname = input("Enter your surname:")
+age = int(input("Enter your age:"))
+favourite_number = float(input("What is your favourite number:"))
+full_name = first_name + " " + surname
+print(f"Welcome , {full_name} !")
---
+++ lesson1.py
@@ -0,0 +1,10 @@
+# making comoputer talk
+print("hello world")
+
+# storing data in variables
+name = "Mark"
+print(name)
+
+# making our code interactive
+name = input("what is your name? ")
+print (f"Greetings ,{name}")
